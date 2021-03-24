ac7ab6fd28ec78beae3743a4cd5399779835c1dc
gadeakhilreddy <reddygadeakhil@gmail.com>
2021-03-24 15:37:34 +0530

    added comments

--- SpringTransaction/src/main/java/com/example/springRestConsumer/AppController.java
+++ SpringTransaction/src/main/java/com/example/springRestConsumer/AppController.java
@@ -1,6 +1,6 @@
 package com.example.springRestConsumer;
 
-
+//this is appp cohntroller
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
